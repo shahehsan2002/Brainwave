790b0a1995b6c169dc4109d091dd175cd559e970
shahehsan2002 <shahehsan2002@.com>
2024-03-02 22:39:49 +0600

    header area created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
-
-function App() {
-
-
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl text-center bg-red-100" >Hello World</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
+        <Header></Header>
+
+      </div>
+      <ButtonGradient></ButtonGradient>
     </>
   )
 }
