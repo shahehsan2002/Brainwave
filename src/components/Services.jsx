d16b9675e470f74426d73866e4127591aa282923
shahehsan2002 <shahehsan2002@.com>
2024-03-20 02:35:20 +0600

    second service area done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants"
 import Generating from "./Generating";
+import { PhotoChatMessage, Gradient, VideoBar, VideoChatMessage } from "./design/Services";
 const Services = () => {
   return (
     <Section id="how-to-use" >
@@ -12,6 +13,8 @@ const Services = () => {
           text="Brainwave unlocks the potential of AI-powered applications"
         />
         <div className="relative" >
+          {/* First Service Section */}
+
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]" >
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto" >
               <img
@@ -42,6 +45,25 @@ const Services = () => {
               </ul>
             </div>
             <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2 " />
+          </div>
+          {/* Second Service Section */}
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2" >
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden" >
+              <div className="absolute inset-0" >
+                <img src={service2}
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot" />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15 " >
+                <h4 className="h4 mb-4 " >Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3" >Automatically enhance your photos using our AI app&apos;s photo editing feature. Try it now!</p>
+              </div>
+
+              <PhotoChatMessage/>
+            </div>
           </div>
         </div>
       </div>
