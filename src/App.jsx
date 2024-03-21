f8bc4d9c676d73f9185654832d19819e9e16cac9
shahehsan2002 <shahehsan2002@.com>
2024-03-21 23:51:26 +0600

    Roadmap Section done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Collaboration/>
         <Services/>
         <Pricing/>
+        <Roadmap/>
         <Footer/>
       </div>
       <ButtonGradient></ButtonGradient>
